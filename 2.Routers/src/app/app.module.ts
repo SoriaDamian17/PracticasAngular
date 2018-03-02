677bc5f739d03ed1176cd9539930f64bb6eaaf1c
Damian Soria <damian.soria.web@gmail.com>
2018-03-01 23:25:50 -0300

    Upload Practica Router

---
+++ 2.Routers/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+const routes:Routes = [
+  {path:'', component:AboutComponent},
+  {path:'Contact', component:ContactComponent},
+];
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
