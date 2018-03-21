06dc313c1cca8f54fb6461ac87e5395ad59a59ac
Damian Soria <damian.soria.web@gmail.com>
2018-03-21 09:51:40 -0300

    'Add_services'

--- 5.Proveedores/src/app/app-routing.module.ts
+++ 5.Proveedores/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContratosComponent } from './components/contratos/contratos.component';
+import { LoginComponent } from './components/login/login.component';
+import { FormularioComponent } from './components/proveedores/formulario/formulario.component';
+import { ListadoComponent } from './components/proveedores/listado/listado.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'Ingreso',
+    component: LoginComponent
+  },
+  {
+    path:'Presupuestos',
+    component: ListadoComponent
+  },
+  {
+    path:'Presupuesto/Alta',
+    component: FormularioComponent
+  },
+  {
+    path:'Contratos',
+    component: ContratosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
